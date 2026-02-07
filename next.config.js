39073e58919ebbd219ab1367ea78480a2d0948ba
kazuma <ka3zu1ma10@gmail.com>
2026-02-07 15:43:52 +0900

    リセット

--- next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  reactStrictMode: true,
-  // GitHub Pages configuration
-  basePath: process.env.NODE_ENV === 'production' ? '/ka-zu-ma' : undefined,
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/ka-zu-ma/' : undefined,
-  trailingSlash: true,
-}
